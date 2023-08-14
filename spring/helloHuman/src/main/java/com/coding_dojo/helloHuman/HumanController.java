4b03b7fa8bfe0c36c5b715d571e0c81f3b7ff048
Autumn Han <autumn9894@gmail.com>
2023-08-14 12:14:29 -0700

    added another query parameter to helloHuman

--- spring/helloHuman/src/main/java/com/coding_dojo/helloHuman/HumanController.java
+++ spring/helloHuman/src/main/java/com/coding_dojo/helloHuman/HumanController.java
@@ -8,18 +8,40 @@ import org.springframework.web.bind.annotation.RestController;
 public class HumanController {
 	@RequestMapping("/")
 	public String index(
-			@RequestParam(value="name", required=false) String name,
-			@RequestParam(value="last_name", required=false) String last_name) {
-		if (name != null && last_name != null) {
-			return "Hello " + name + " " + last_name;
-		}
-		else if (name != null && last_name == null) {
-			return "Hello " + name;
-		}
-		else {
-			return "Hello Human";
-		}
+		@RequestParam(value="name", required=false) String name,
+		@RequestParam(value="last_name", required=false) String last_name,
+		@RequestParam(value="times", required=false) Integer times) {
+			if (times != null && times > 0) {
+				String greeting = "";
+				for (int i = 0; i < times; i++) {
+					if (name != null && last_name != null) {
+						greeting += "Hello " + name + " " + last_name + " ";
+					}
+					else if (name != null) {
+						greeting += "Hello " + name + " ";
+					}
+					else if (last_name != null) {
+						greeting += "Hello " + last_name + " ";
+					}
+					else {
+						greeting += "Hello Human";
+					}
+				}
+				return greeting;
+			}
+			else {
+				if (name != null && last_name != null) {
+					return "Hello " + name + " " + last_name;
+				}
+				else if (name != null) {
+					return "Hello " + name;
+				}
+				else if (last_name != null) {
+					return "Hello " + last_name;
+				}
+				else {
+					return "Hello Human";
+				}
+			}
 	}
-	
-
 }
