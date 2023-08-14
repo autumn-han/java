05ad6438c45396ca86d3ca2093dec4fe3702c3d6
Autumn Han <autumn9894@gmail.com>
2023-08-14 10:56:53 -0700

    initial commit for daikichi

---
+++ spring/daikichi/src/test/java/com/coding_dojo/daikichi/DaikichiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coding_dojo.daikichi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaikichiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
