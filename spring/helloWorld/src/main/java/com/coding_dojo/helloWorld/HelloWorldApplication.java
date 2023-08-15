d14fddab9227853339d8aceecf5ed17707427b58
Autumn Han <autumn9894@gmail.com>
2023-08-15 14:14:07 -0700

    learning MVC framework for SpringBoot projects

--- spring/helloWorld/src/main/java/com/coding_dojo/helloWorld/HelloWorldApplication.java
+++ spring/helloWorld/src/main/java/com/coding_dojo/helloWorld/HelloWorldApplication.java
@@ -2,26 +2,12 @@ package com.coding_dojo.helloWorld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
-@RestController
-
 public class HelloWorldApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldApplication.class, args);
 	}
-	
-	@RequestMapping("/")
-	public String index() {
-		return "Hello World";
-	}
-	
-	@RequestMapping("/new_route")
-	public String my_own() {
-		return "Houston we don't have a problem";
-	}
-
 }
