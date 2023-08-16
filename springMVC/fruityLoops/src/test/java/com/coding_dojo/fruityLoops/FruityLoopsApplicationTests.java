815abafa59cf1ae25958c10b50a32645be212533
Autumn Han <autumn9894@gmail.com>
2023-08-15 18:42:11 -0700

    initial commit for fruityLoops

---
+++ springMVC/fruityLoops/src/test/java/com/coding_dojo/fruityLoops/FruityLoopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coding_dojo.fruityLoops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruityLoopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
