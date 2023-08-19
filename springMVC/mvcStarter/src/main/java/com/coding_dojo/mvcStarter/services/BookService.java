327bd40295c763efda76103be8de185a75bbec43
Autumn Han <autumn9894@gmail.com>
2023-08-19 16:02:32 -0700

    added create and retrieve methods to app

--- springMVC/mvcStarter/src/main/java/com/coding_dojo/mvcStarter/services/BookService.java
+++ springMVC/mvcStarter/src/main/java/com/coding_dojo/mvcStarter/services/BookService.java
@@ -1,8 +1,39 @@
 package com.coding_dojo.mvcStarter.services;
 
+import java.util.List;
+import java.util.Optional;
 import org.springframework.stereotype.Service;
+import com.coding_dojo.mvcStarter.models.Book;
+import com.coding_dojo.mvcStarter.repositories.BookRepository;
 
 @Service
 public class BookService {
-
+	
+//	this adds bookRepository as a dependency
+	private final BookRepository bookRepository;
+	
+	public BookService(BookRepository bookRepository) {
+		this.bookRepository = bookRepository;
+	}
+	
+//	returns ALL books
+	public List<Book> allBooks() {
+		return bookRepository.findAll();
+	}
+	
+//	creates a book
+	public Book createBook(Book b) {
+		return bookRepository.save(b);
+	}
+	
+//	retrieves a book
+	public Book findBook(Long id) {
+		Optional<Book> optionalBook = bookRepository.findById(id);
+		if (optionalBook.isPresent()) {
+			return optionalBook.get();
+		}
+		else {
+			return null;
+		}
+	}
 }
