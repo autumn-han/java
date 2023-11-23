ca10210d51ee8515724c15270b295f0c0b84ac6d
Autumn Han <autumn9894@gmail.com>
2023-11-23 12:20:50 -0800

    reviewing Java fundamentals

---
+++ fundamentals/Main.java
@@ -0,0 +1,13 @@
+// this file is explicitly for testing basic functionalities of Java
+
+// this is a comment
+/* this is a 
+multi-line comment */
+
+public class Main {
+    public static void main(String[] args) {
+        double myDouble = 9.7824d;
+        int myInt = (int) myDouble;
+        System.out.println(myInt);
+    }
+}
