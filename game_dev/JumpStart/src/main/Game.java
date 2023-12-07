397e5f941a47fcbf269eccfd854af45fd43a29a9
Autumn Han <autumn9894@gmail.com>
2023-12-07 14:10:01 -0800

    modularized regulating updates and frame rendering in Game class

--- game_dev/JumpStart/src/main/Game.java
+++ game_dev/JumpStart/src/main/Game.java
@@ -6,6 +6,7 @@ public class Game implements Runnable {
 	private GamePanel gamePanel;
 	private Thread gameThread;
 	private final int FPS_SET = 120;
+	private final int UPS_SET = 200;
 	
 	public Game() {
 		gamePanel = new GamePanel();
@@ -23,25 +24,40 @@ public class Game implements Runnable {
 	public void run() {
 		
 		double timePerFrame = 1000000000.0 / FPS_SET;
-		long lastFrame = System.nanoTime();
-		long now = System.nanoTime();
+		double timePerUpdate = 1000000000.0 / UPS_SET;
 		
 		int frames = 0;
+		int updates = 0;
 		long lastCheck = System.currentTimeMillis();
+		long previousTime = System.nanoTime();
+		
+		double deltaU = 0;
+		double deltaF = 0;
 		
 		while (true) {
 			
-			now = System.nanoTime();
-			if (now - lastFrame >= timePerFrame) {		
+			long currentTime = System.nanoTime();
+			deltaU += (currentTime - previousTime) / timePerUpdate;
+			deltaF += (currentTime - previousTime) / timePerFrame;
+			previousTime = currentTime;
+			
+			if (deltaU >= 1) {
+				// udpate();
+				updates++;
+				deltaU--;
+			}
+			
+			if (deltaF >= 1) {		
 				gamePanel.repaint();
-				lastFrame = now;
 				frames++;
+				deltaF--;
 			}
 			
 			if (System.currentTimeMillis() - lastCheck >= 1000) {
 				lastCheck = System.currentTimeMillis();
-				System.out.println("FPS: " + frames);
+				System.out.println("FPS: " + frames + " | " + "UPS: " + updates);
 				frames = 0;
+				updates = 0;
 			}
 		}
 		
