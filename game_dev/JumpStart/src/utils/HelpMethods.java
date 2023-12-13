5734ac31c90a2024f5830faa85fdfb9fb6716e9b
Autumn Han <autumn9894@gmail.com>
2023-12-13 11:03:43 -0800

    added helpmethods class to utils package to validate whether there is a tile in the game panel

--- game_dev/JumpStart/src/utils/HelpMethods.java
+++ game_dev/JumpStart/src/utils/HelpMethods.java
@@ -1,5 +1,40 @@
 package utils;
 
+import main.Game;
+
 public class HelpMethods {
 
+	public static boolean canMoveHere(float x, float y, int width, int height, int[][] levelData) {
+		if (!isSolid(x, y, levelData)) {
+			if (!isSolid(x + width, y + height, levelData)) {
+				if (!isSolid(x + width, y, levelData)) {
+					if (!isSolid(x, y + height, levelData)) {
+						return true;
+					}
+				}
+			}
+		}
+		return false;
+	}
+	
+	public static boolean isSolid(float x, float y, int[][] levelData) {
+		
+		if (x < 0 || x > Game.GAME_WIDTH) {
+			return true;
+		}
+		
+		if (y < 0 || y > Game.GAME_HEIGHT) {
+			return true;
+		}
+		
+		float xIndex = x / Game.GAME_WIDTH;
+		float yIndex = y / Game.GAME_HEIGHT;
+		int value = levelData[(int) xIndex][(int) yIndex];
+		
+		if (value >= 242 || value < 0) {
+			return true;
+		}
+		
+		return false;
+	}
 }
