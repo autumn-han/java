1043f16f9c506304e2f2269e9dccc7ff0087c377
Autumn Han <autumn9894@gmail.com>
2023-12-13 17:48:59 -0800

    troubleshooting the collision detector; still not recogniziing filled tiles

--- game_dev/JumpStart/src/utils/HelpMethods.java
+++ game_dev/JumpStart/src/utils/HelpMethods.java
@@ -14,21 +14,25 @@ public class HelpMethods {
 	}
 	
 	private static boolean isSolid(float x, float y, int[][] levelData) {
-		
-		if (x < 0 || x >= Game.GAME_WIDTH)
-			return true;
-		
-		if (y < 0 || y >= Game.GAME_HEIGHT)
-			return true;
-		
 		float xIndex = x / Game.TILES_SIZE;
 		float yIndex = y / Game.TILES_SIZE;
-		
 		int value = levelData[(int) yIndex][(int) xIndex];
+		System.out.println("yIndex: " + yIndex + " | xIndex: " + xIndex);
 		
-		if (value >= 242 || value < 0)
+		if (x < 0 || x >= Game.GAME_WIDTH) {
+			System.out.println("YOU'RE AT THE EDGE OF THE WORLD" + " | " + x);
 			return true;
+		}
 		
+		if (y < 0 || y >= Game.GAME_HEIGHT) {
+			System.out.println("YOU'RE AT THE TOP OR BOTTOM" + " | " + y);
+			return true;
+		}
+		
+		if (value >= 242 || value < 0) {
+			System.out.println("YOU SHALL NOT PASS" + " | " + value);
+			return true;
+		}
 		return false;
 	}
 }
