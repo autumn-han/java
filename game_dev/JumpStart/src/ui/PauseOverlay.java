e76613f8d85eae118b9ea91b907965fda59376d7
Autumn Han <autumn9894@gmail.com>
2023-12-15 11:23:40 -0800

    added sound buttons to the pause menu

--- game_dev/JumpStart/src/ui/PauseOverlay.java
+++ game_dev/JumpStart/src/ui/PauseOverlay.java
@@ -6,23 +6,34 @@ import java.awt.image.BufferedImage;
 
 import main.Game;
 import utils.LoadSave;
+import static utils.Constants.UI.PauseButtons.*;
 
 public class PauseOverlay {
 	
 	private BufferedImage backgroundImg;
 	private int bgX, bgY, bgW, bgH;
+	private SoundButton musicButton, sfxButton;
 	
 	public PauseOverlay() {
 		loadBackground();
+		createSoundButtons();
 		
 	}
 	
+	private void createSoundButtons() {
+		int soundX = (int) (450 * Game.SCALE);
+		int musicY = (int) (140 * Game.SCALE);
+		int sfxY = (int) (186 * Game.SCALE);
+		musicButton = new SoundButton(soundX, musicY, SOUND_SIZE, SOUND_SIZE);
+		sfxButton = new SoundButton(soundX, sfxY, SOUND_SIZE, SOUND_SIZE);
+	}
+
 	private void loadBackground() {
 		backgroundImg = LoadSave.getSpriteAtlas(LoadSave.PAUSE_BACKGROUND);
 		bgW = (int) (backgroundImg.getWidth() * Game.SCALE);
 		bgH = (int) (backgroundImg.getHeight() * Game.SCALE);
 		bgX = Game.GAME_WIDTH / 2 - bgW / 2;
-		bgY = 50;
+		bgY = (int) (25 * Game.SCALE);
 		
 	}
 
@@ -31,7 +42,12 @@ public class PauseOverlay {
 	}
 	
 	public void draw(Graphics g) {
+		// background menu
 		g.drawImage(backgroundImg, bgX, bgY, bgW, bgH, null);
+		
+		// sound buttons
+		musicButton.draw(g);
+		sfxButton.draw(g);
 		
 	}
 	
